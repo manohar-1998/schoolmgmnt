3f11ecc358d4804ae8956e261183845bfffabfdf
manohar-1998 <gorlamanohar98@gmail.com>
2021-02-27 16:42:50 +0530

    My Project

---
+++ samplehtml.js
@@ -0,0 +1,36 @@
+<body>
+<div class="container">
+        <br><br><br><br>
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form method="post">
+                            <fieldset>
+                                <div class="form-group">
+             <input class="form-control" placeholder="Login Id"  id="id"name="id" type="text" autofocus autocomplete="off">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" id="password"name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                    </label>
+                                    
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Forgot Password
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <input type="submit" value="login" name="submit" class="btn btn-lg btn-success btn-block">
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
